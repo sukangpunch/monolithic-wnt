f0139865f9ed47e2e7aac9ccaf81b3c58bd23e92
sukangpunch <popora99@naver.com>
2025-07-27 16:36:33 +0900

    init: 모놀로틱 아키텍처 서버 초기화

---
+++ src/main/java/com/appcenter/monolithicwnt/MonolithicWntApplication.java
@@ -0,0 +1,13 @@
+package com.appcenter.monolithicwnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithicWntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonolithicWntApplication.class, args);
+    }
+
+}
